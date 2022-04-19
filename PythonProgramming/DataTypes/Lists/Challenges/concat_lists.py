8e92c14e12a32b41b0f0c8b5e66254733604d078
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-04-18 20:30:45 -0700

    Update concat_lists.py

--- PythonProgramming/DataTypes/Lists/Challenges/concat_lists.py
+++ PythonProgramming/DataTypes/Lists/Challenges/concat_lists.py
@@ -1,4 +1,8 @@
+# PROMPT: Use a list comprehension to iterate two lists using a for loop and concatenate the current item of each list.
+
+
 def list_combinations(lst1, lst2):
 	return [x+ ' ' +y for x in lst1 for y in lst2]
+
 
 print(list_combinations(['New York', 'San Francisco'], ['Knicks', 'Warriors']))
