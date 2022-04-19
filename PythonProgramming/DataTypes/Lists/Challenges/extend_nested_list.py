8a6ff9b6077301001efe4d5f0ab4653aff00e0b3
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-04-18 21:42:40 -0700

    Create extend_nested_list.py

---
+++ PythonProgramming/DataTypes/Lists/Challenges/extend_nested_list.py
@@ -0,0 +1,7 @@
+# add a new key / value pair to a specified position in a nested list
+
+original_list = ['food', [89, 56], 44.7, {'SF': 99, 'DEN': 22, 'ATL': 9}, [300, 400]]
+original_list[3]['NYC'] = 7
+
+
+print(original_list)
