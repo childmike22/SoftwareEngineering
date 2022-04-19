13099b5f6900e6480d4b401d38a262d47768c01a
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-04-18 19:54:44 -0700

    Update reversed_list.py

--- PythonProgramming/DataTypes/Lists/Challenges/reversed_list.py
+++ PythonProgramming/DataTypes/Lists/Challenges/reversed_list.py
@@ -1,4 +1,15 @@
+# option 1
 def reverse_list(lst):
 	return lst[::-1]
 
+
+# option 2
+def rev_list(lst):
+	lst.reverse()
+	return lst
+
+
 print(reverse_list([44,55,90, 'cat', 'in']))
+
+
+print(rev_list(['p', 33, 90]))
