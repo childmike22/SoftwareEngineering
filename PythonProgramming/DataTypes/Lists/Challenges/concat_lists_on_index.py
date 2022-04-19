f882d3ffe0e88be8ec1b3fc21b1ecbfbadbaaaad
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-04-18 20:29:00 -0700

    Update concat_lists_on_index.py

--- PythonProgramming/DataTypes/Lists/Challenges/concat_lists_on_index.py
+++ PythonProgramming/DataTypes/Lists/Challenges/concat_lists_on_index.py
@@ -1,3 +1,7 @@
+# PROMPT: Write a program to add two lists index-wise. Create a new list that contains the 0th index item from both the list, 
+# then the 1st index item, and so on till the last element.
+# any leftover items will get added at the end of the new list.
+
 
 def concat_lists_index_wise(lst1, lst2):
 	list3 = [i+j for i,j in zip(lst1, lst2)]
