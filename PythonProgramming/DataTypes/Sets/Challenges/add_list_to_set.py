586bae7317bed97badfdb41f6679d409f454ec30
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-04-24 23:33:47 -0700

    Create add_list_to_set.py

---
+++ PythonProgramming/DataTypes/Sets/Challenges/add_list_to_set.py
@@ -0,0 +1,9 @@
+# take a list and add it's elements to a set
+color_set = {'blue', 'green', 'yellow'}
+
+color_list = ['blue', 'orange', 'green', 'blue']
+
+
+# use update to add elements to a set
+color_set.update(color_list)
+print(color_set)
